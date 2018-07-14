2f381ddbaf2dce61db53c6469ca1b0577a2fad38
Borodor <deepanshuwadhwa97@gmail.com>
2018-07-14 19:23:50 +0530

    Table creation code edited

--- table_creation.py
+++ table_creation.py
@@ -13,48 +13,48 @@ cur = dB.cursor()
 
 # Users table
 
-cur.execute('''CREATE table users (id INTEGER AUTO INCREMENT, cpf INTEGER \
-	PRIMARY KEY, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
+cur.execute('''CREATE table users (id INTEGER PRIMARY KEY AUTOINCREMENT, cpf INTEGER \
+	, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
 	 REQUIRED, contact NUMBER REQUIRED, department TEXT REQUIRED)''')
 
 dB.commit()
 
 # Complaints table
 
-cur.execute(''' CREATE table complaints (id INTEGER AUTO INCREMENT, num TEXT\
- PRIMARY KEY, cpf INTEGER REQUIRED, from_deptt TEXT REQUIRED, for_deptt TEXT\
+cur.execute(''' CREATE table complaints (id INTEGER PRIMARY KEY AUTOINCREMENT, num TEXT\
+ , cpf INTEGER REQUIRED, from_deptt TEXT REQUIRED, for_deptt TEXT\
   REQUIRED, content TEXT REQUIRED, comp_time time REQUIRED)''')
 
 dB.commit()
 
 # Complaint status table
 
-cur.execute('''CREATE table status (id INTEGER AUTO INCREMENT, comp_num TEXT\
- PRIMARY KEY, created_on time REQUIRED, approved_on time, under_process_on\
+cur.execute('''CREATE table status (id INTEGER AUTO INCREMENT PRIMARY KEY, comp_num TEXT\
+ , created_on time REQUIRED, approved_on time, under_process_on\
   time, resolved_on time)''')
 
 dB.commit()
 
 # Session table
 
-cur.execute('''CREATE table sessions (id INTEGER AUTO INCREMENT, sess_id TEXT\
- PRIMARY KEY, cpf NUMBER REQUIRED, role INTEGER REQUIRED, log_in time\
+cur.execute('''CREATE table sessions (id INTEGER AUTO INCREMENT PRIMARY KEY, sess_id TEXT\
+ , cpf NUMBER REQUIRED, role INTEGER REQUIRED, log_in time\
   REQUIRED, active INTEGER REQUIRED)''')
 
 dB.commit()
 
 # Department heads table
 
-cur.execute('''CREATE table dheads (id INTEGER AUTO INCREMENT, cpf INTEGER \
-	PRIMARY KEY, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
+cur.execute('''CREATE table dheads (id INTEGER AUTO INCREMENT PRIMARY KEY , cpf INTEGER \
+	, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
 	 REQUIRED, contact NUMBER REQUIRED, department TEXT REQUIRED)''')
 
 dB.commit()
 
 # Complaint heads table
 
-cur.execute('''CREATE table cheads (id INTEGER AUTO INCREMENT, cpf INTEGER \
-	PRIMARY KEY, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
+cur.execute('''CREATE table cheads (id INTEGER AUTO INCREMENT PRIMARY KEY , cpf INTEGER \
+	, first_name TEXT REQUIRED, surname TEXT REQUIRED, email TEXT\
 	 REQUIRED, contact NUMBER REQUIRED, department TEXT REQUIRED)''')
 
 dB.commit()
